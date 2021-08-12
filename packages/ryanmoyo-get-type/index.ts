a52bb9241c8f35025f8018d6c13969c04c5dc9e3
ryanmoyo <iherewithmyheart@gmail.com>
2021-08-12 11:13:50 +0800

    🔥 rm -rf shit code

--- packages/ryanmoyo-get-type/index.ts
+++
@@ -1,5 +0,0 @@
-const getType = (value : any) : string => Object.prototype.toString.call(value)
-
-const getLowercaseType =  (value : any) : string => getType(value).slice(8,-1).toLowerCase()
-
-export {getType,getLowercaseType}
